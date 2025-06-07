626e8400237a0e9cce6dea6a2fb9f0f08a60e261
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-06-07 21:52:28 +0530

    This is the initial commit for upgraded portfolio along with new dedicated project page.
    The website and the project page, both are not responsive right now.
    I'm working on it and soon they'll be there.

--- script.js
+++
@@ -1,6 +0,0 @@
-function toogle_menu(){
-    const menu = document.querySelector(".menulinks")
-    const icon = document.querySelector(".hamicon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
